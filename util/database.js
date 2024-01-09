2e42de21a274de248fcefa7248445145b8349d68
Jihad Abdulrazaq <jihad.abdulrazaq@gmail.com>
2024-01-10 00:21:48 +0300

    database connection setup file

--- util/database.js
+++ util/database.js
@@ -1,10 +1,5 @@
-const mysql = require('mysql2')
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    password:'jihad@1234',
-    database:'node-app',
-})
+const Sequelize = require('sequelize')
 
+const sequelize = new Sequelize('node-app', 'root', 'jihad@1234', { dialect: 'mysql', host: 'localhost' })
 
-module.exports = pool.promise()
+module.exports = sequelize
