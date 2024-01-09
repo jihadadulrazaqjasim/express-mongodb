1be1c777194131a8b06e553af2183ba488546931
Jihad Abdulrazaq <jihad.abdulrazaq@gmail.com>
2024-01-10 00:19:06 +0300

    shop related (controller, models and routes)

--- app.js
+++ app.js
@@ -1,26 +1,84 @@
-//packages
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
+// packages
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const {faker} = require('@faker-js/faker')
 
-//local
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// local
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 const errorController = require('./controllers/error')
-const app = express();
+const sequelize = require('./util/database')
 
-app.set('view engine','ejs')
+// Models
+const User = require('./models/user')
+const Product = require('./models/product')
+const Cart = require('./models/cart')
+const CartItem = require('./models/cart-item')
+const {Sequelize} = require('sequelize')
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
+
+const app = express()
+app.set('view engine', 'ejs')
 // app.set('views','views') //Not needed
 
-//parse body
-app.use(bodyParser.urlencoded({extended: false}));
+// parse body
+app.use(bodyParser.urlencoded({extended: false}))
 
-//serve static content from public folder
-app.use(express.static(path.join(__dirname, 'public')));
+// serve static content from public folder
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// middleware to store user in req:
+app.use((req, res, next) => {
+    User.findAll({order: Sequelize.literal('rand()'), limit: 1})
+        .then(users => {
+            req.user = users[0]
+            next()
+        }).catch()
+})
 
-app.use(errorController.get404);
+// Routes
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+app.use(errorController.get404)
 
-app.listen(3000);
+// Define relationship:
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE', foreignKey: 'createdBy'})
+User.hasMany(Product, {foreignKey: 'createdBy'})
+
+Cart.belongsTo(User)
+User.hasOne(Cart)
+
+Cart.belongsToMany(Product, {through: CartItem})
+Product.belongsToMany(Cart, {through: CartItem})
+
+//User & Order
+User.hasMany(Order, {constraints: true, onDelete: 'CASCADE'})
+Order.belongsTo(User)
+
+
+//Order & Product
+Order.belongsToMany(Product, {through: OrderItem})
+Product.belongsToMany(Order, {through: OrderItem})
+
+sequelize
+    // .sync({force: true})
+    .sync()
+    .then((result) => {
+        return User.findAll({order: Sequelize.literal('rand()'), limit: 1})
+        // return User.findOrCreate({where: {id: 1},defaults:{name:'Jihad',email:'jihadabdulrazaq@gmail.com'}})
+    })
+    .then(users => {
+        if (users[0] == null) {
+            return User.create({name: faker.person.firstName(), email: faker.internet.email()})
+        }
+        return Promise.resolve(users[0])
+    })
+    .then(user => {
+        // return user.createCart()
+    })
+    .then(result => {
+        app.listen(3000)
+    })
+    .catch(err => console.log(err))
--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,17 @@
-//packages
-const express = require('express');
-const router = express.Router();
+// packages
+const express = require('express')
+const router = express.Router()
 
-//local files
+// local files
 const shopController = require('../controllers/shop')
-router.get('/', shopController.getIndex);
-router.get('/products/:productId', shopController.show);
-router.get('/products',shopController.getProducts)
-router.get('/cart',shopController.cart)
-router.post('/cart/deleteFromCart/:id',shopController.deleteFromCart)
-router.post('/cart',shopController.addToCart)
-router.get('/checkout',shopController.checkout)
-router.get('/orders',shopController.getOrders)
+router.get('/', shopController.getIndex)
+router.get('/products/:productId', shopController.show)
+router.get('/products', shopController.getProducts)
+router.get('/cart', shopController.cart)
+router.post('/cart/deleteFromCart/:id', shopController.deleteFromCart)
+router.post('/cart', shopController.addToCart)
+router.get('/checkout', shopController.checkout)
+router.get('/orders', shopController.getOrders)
+router.post('/orders', shopController.storeOrder)
 
-module.exports = router;
+module.exports = router
